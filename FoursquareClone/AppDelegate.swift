114cdfddc6ec2dba5acccb04a9313739e19d2919
onurbasdas <onurebasdase@hotmail.com>
2021-01-29 16:49:52 +0300

    parse save data created

--- FoursquareClone/AppDelegate.swift
+++ FoursquareClone/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let configuration = ParseClientConfiguration{ (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "qSeh3D6cxsoxEw8eO0H2RzegRZNK1kebYkG6n8EK"
+            ParseMutableClientConfiguration.clientKey = "d1IWI4tVILRiKuuq0EbzThYqTudpnJvXZITICAYd"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: configuration)
         return true
     }
 
